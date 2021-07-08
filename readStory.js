4dd63c99b9e9657e7a5389f02c97c15b624b3c26
Nishika11Singhvi <73836493+Nishika11Singhvi@users.noreply.github.com>
2021-07-08 20:14:00 +0530

    Add files via upload

---
+++ readStory.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+
+export default class ReadScreen extends React.Component 
+{
+  render() 
+  {
+    return (
+      <View style = {{ justifyContent: 'center', alignItems: 'center', marginTop : 150,}}>
+
+        
+
+      </View>
+      )
+    }
+}
